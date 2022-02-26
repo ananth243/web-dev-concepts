6368a616d05bd6735e83c35240dfeff57a93cdd4
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2022-02-26 15:47:15 +0530

    Design page component
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- components/Page.js
+++ components/Page.js
@@ -2,14 +2,17 @@ import React from "react";
 
 function Page({ title, description, problem, execute, message }) {
   return (
-    <>
-      <h1>{title}</h1>
+    <div className="flex align-center flex-col p-4 space-y-2">
+      <h1 className="text-5xl uppercase text-center mb-3 font-light">{title}</h1>
       {description()}
       {problem()}
-      <button onClick={() => execute()}>Execute</button>
-      {message.status && <h1 className="text-white">{message.message}</h1>}
-      {!message.status && <h1 className="text-red-600">{message.message}</h1>}
-    </>
+      <div className="flex items-center space-x-3">
+        <button className="w-min py-1 px-3 rounded border-2 border-blue-400" onClick={() => execute()}>Execute</button>
+        {message.message && (<div className={`${message.status ? "bg-green-500" : "bg-red-500"} text-white py-1 px-3 rounded-md flex-grow`}>
+          Result: {message.message}
+        </div>)}
+      </div>
+    </div>
   );
 }
 
